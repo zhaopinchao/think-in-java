780bb8b2f7d878281b228967ed3f84b2344a0eb2
zhaopinchao <17688786060@sina.cn>
2019-06-22 09:15:50 +0800

    annotation

---
+++ src/annotation/UseCaseTracker.java
@@ -0,0 +1,30 @@
+package annotation;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class UseCaseTracker {
+
+    public static void trackUseCase(List<Integer> useCase, Class<?> cl) {
+
+        for (Method m : cl.getDeclaredMethods()) {
+            UseCase uc = m.getAnnotation(UseCase.class);
+            if (uc != null) {
+                System.out.println("Found Use Case: " + uc.id() + " " + uc.description());
+                useCase.remove(new Integer(uc.id()));
+            }
+        }
+        for (int i : useCase) {
+            System.out.println("Warning: Missing use case=" + i);
+        }
+    } 
+
+    public static void main(String[] args) {
+        List<Integer> useCase = new ArrayList<>();
+        Collections.addAll(useCase, 47, 48, 49, 50);
+        trackUseCase(useCase, PasswordUtils.class);
+    }
+}
